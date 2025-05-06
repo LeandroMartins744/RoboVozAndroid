d72c69437a528c67192359ad6e088b8709e4946c
Leandro Silva <lndmrt6@gmail.com>
2025-05-06 20:22:44 -0300

    feat: notification

--- app/src/main/java/com/example/myapplication/util/Constants.kt
+++ app/src/main/java/com/example/myapplication/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.util
 
-const val BASE_URL = "http://systechbrasil.vps-uni5.net:8080/api/"
+const val BASE_URL = "http://192.168.15.16:8080/api/"
+//const val BASE_URL = "http://systechbrasil.vps-uni5.net:8080/api/"
 const val USER_REST = "leandro@teste.com"
 const val PASS_REST = "nota10"
 const val DATA_DB_USERS_LOCAL = "dbUsersLocal"
